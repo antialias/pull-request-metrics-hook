38f1899c5c2f633ceec1a918f7f9461e3f6ec12f
antialias <thomas.hallock@compass.com>
2019-11-20 07:16:15 -0800

    using datadog rest api instead of datadog agent layer because IAM profile for datadog hasn't been configured for the ddagent lambda layer

--- src/app.js
+++ src/app.js
@@ -1,35 +1,17 @@
+/* global Promise */
 import "dotenv-safe/config";
 import bodyParser from "koa-bodyparser";
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { createHttpLink } from "apollo-link-http";
 import Koa from "koa";
-import * as queries from "./queries";
-import fetch from "node-fetch";
-import HotShots from "hot-shots";
 import { set } from "object-path-immutable";
+import getUnixTime from "date-fns/getUnixTime";
+import axios from "axios";
 
-const statsd = new HotShots({
-  prefix: "pull-requests",
+import * as queries from "./queries";
+import client from "./apollo-client";
+
+const ddSeries = axios.create({
+  baseURL: `https://api.datadoghq.com/api/v1/series?api_key=${process.env.datadogApiKey}`,
 });
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: createHttpLink({
-    fetch,
-    uri: process.env.githubGraphQLApi,
-    headers: {
-      Authorization: `bearer ${process.env.githubToken}`,
-    },
-  }),
-  name: "github-graphql-api",
-  defaultOptions: {
-    query: {
-      fetchPolicy: "no-cache",
-    },
-  },
-});
-
 const app = new Koa();
 app.use(bodyParser());
 app.use(async ctx => {
@@ -52,14 +34,36 @@ app.use(async ctx => {
         },
       })
     ).data.repository);
-    pullRequests.edges.forEach(({ node: pr }) =>
-      statsd.set(
-        process.env.metricName,
-        `${pr.baseRepository.nameWithOwner}/pull/${pr.number}`,
-        {
-          state: pr.state,
-          author: pr.author.login,
-        },
+    const counts = pullRequests.edges.reduce(
+      (acc, { node: pr }) => ({
+        ...acc,
+        [pr.state]: acc[pr.state] ? acc[pr.state] + 1 : 1,
+      }),
+      {},
+    );
+    const repos = Object.entries(
+      pullRequests.edges.reduce(
+        (acc, { node: pr }) => ({
+          ...acc,
+          [pr.baseRepository.nameWithOwner]: true,
+        }),
+        {},
+      ),
+    ).forEach(([key, val]) => `${key}:${val}`);
+    const time = getUnixTime(new Date());
+    await Promise.all(
+      Object.entries(counts).map(([state, count]) =>
+        // TODO: replace with query to graphql client via apollo-link-rest
+        ddSeries.post("", {
+          series: [
+            {
+              metric: `hackathon.prcount.${state}`,
+              tags: repos,
+              type: "gauge",
+              points: [[time, count]],
+            },
+          ],
+        }),
       ),
     );
   }
